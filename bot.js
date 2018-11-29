043f4e7baf3d78e65e6697eb6e124fca02ba6c7a
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-29 20:55:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -157,7 +157,8 @@ client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+     stewart.send(`<@${member.user.id}> invited by <@${inviter.id}>`);
    //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   });
+})
 client.login(process.env.BOT_TOKEN); 
