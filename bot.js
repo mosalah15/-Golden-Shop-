2c849dbefbd7c2266bf88204d27b0ab00f5c3f2f
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-29 21:55:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,16 +151,30 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 }
  
 });
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> invited by <@${inviter.id}>`);
+    const stewart = member.guild.channels.find("name", "wlecome");
+     stewart.send(`<@${member.user.id}> invited By  <@${inviter.id}>`);
    //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-})
+  }); 
+});
 client.on("ready", () => {
   function lol() {
     client.guilds.get('517740329219391498').roles.find("name", "Owner").setColor("RANDOM");
