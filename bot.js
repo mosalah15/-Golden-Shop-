d9ef882d697adbfe014cb83fd6781d7dc67d7fd1
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-29 19:28:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,18 +98,6 @@ msg.delete();
 })
 }
 });
-const Discord = require('discord.js');
-const moment = require("moment");  
-const fs = require("fs");      
-const dateFormat = require('dateformat');
-const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
-const prefix = "!" // برفكس
-let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
-
-
-
-
 client.on("message", message => {
     if (message.author.bot || !message.guild) return; 
     let score;
