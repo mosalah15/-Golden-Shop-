92c16213177e4fbde315f7f5baa4db3f13928e70
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-30 09:11:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,5 +196,28 @@ client.on ("guildMemberRemove", member => {
    
 });
 
+client.on('message', message => {
+    if (message.content.includes('discord.gg')){
+                        if(!message.channel.guild) return message.reply ('')
+                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
+       message.channel.send('ban <@' + message.author.id + '>')
+       message.delete() 
+       }
+    }
+          if (message.content.startsWith("ban ")) {
+             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
+             var member= message.mentions.members.first();
+             member.ban().then((member) => {
+                 message.channel.sendMessage("", {embed: {
+                 author: {
+                 },
+                 title: 'بسبب النشر ' + member.displayName + ' تم حظر',
+                 color: 490101,
+                 }
+               });
+           }
+         ) 
+       }
+   });
 
 client.login(process.env.BOT_TOKEN); 
