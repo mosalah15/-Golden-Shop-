6e3279354376226d0c0feed7da9e11c401c2b70c
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-30 13:20:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,5 +219,15 @@ client.on('message', message => {
          ) 
        }
    });
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Golden Shop`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Golden Shop ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`•Golden Shop•`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+        }
+   });
 client.login(process.env.BOT_TOKEN); 
