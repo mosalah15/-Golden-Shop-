989adfc05dbcefc6f9daf13ed68b48db2cae94a1
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-30 10:56:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,7 +225,7 @@ client.on("message", (message) => {
 if (message.content.startsWith("*cv")) {
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
         let args = message.content.split(" ").slice(1);
-    message.guild.createChannel(args.join(' '), 'voice');
+    message.guild.createChannel(args.join('test '), 'voice');
     message.channel.sendMessage('تـم إنـشاء روم صـوتي')
     
 }
