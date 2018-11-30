bd24b3e22621e277294b0bc64b520d5a8fa87c78
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-30 23:09:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,10 +256,11 @@ client.on('message', async message => {
                     message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
                         credit = collected.first().content;
                         collected.first().delete();
-                        msgS.edit(':timer: | **نوع طلبك؟**').then(msgS => {
-                            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
-                                ordertype = collected.first().content;
-                                collected.first().delete();
+                                msgS.edit(':timer: | **نوع طلبك؟**').then(msgS => {
+                                    message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] }).then(collected => {
+                                        ordertype = collected.first().content;
+                                        collected.first().delete();
+                                       
                                         let embedS = new Discord.RichEmbed()
                                         .setAuthor(message.author.tag, message.author.avatarURL)
                                         .setThumbnail(message.author.avatarURL)
