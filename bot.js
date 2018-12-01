dc93f3bd4ffe35fc69731fbe3b4a2c0d8b09408b
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-01 16:00:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -826,4 +826,30 @@ client.on('message', msg => {
 client.on('ready', () => { //code bot not leave room voice //Bot Is Online
     client.channels.get("517986511527084042").join(); //by : Toxic Codes
     });
+
+client.on("message", (message) => {
+if (message.content.startsWith("*cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
+
+      client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('*ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+                    });
 client.login(process.env.BOT_TOKEN); 
