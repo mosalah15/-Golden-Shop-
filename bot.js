d8a79ba292653e7d196cff29354b31d09c75aa17
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-04 15:37:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -872,7 +872,8 @@ if(message.content === prefix + 'مصحف' || message.content === prefix + 'ms7f
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
 const wait = require('util').promisify(setTimeout);
-  wait(1000);    const ei = invites[member.guild.id];
+  wait(1000);
+  const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "welcome");
